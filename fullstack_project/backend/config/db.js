f2f5a4d140443f864fead8c10869ce8937e38659
aryoshevar-123 <aryosheva.r@gmail.com>
2026-05-19 18:24:31 +0700

    Update db.js

--- fullstack_project/backend/config/db.js
+++ fullstack_project/backend/config/db.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+    if (isConnected) {
+        console.log("=> Use the available database");
+        return;
+    }
+
+    if (!process.env.MONGO_URI) {
+        console.error("Error: MONGO_URI is not found");
+        return;
+    }
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {serverSelectionTimeoutMS: 5000});
+        isConnected = conn.connections[0].readyState;
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error){
         console.error(`Error: ${error.message}`);
-        process.exit(1); // process code 1 means exit with failure, 0 means success
+        throw error;
     }
 };
